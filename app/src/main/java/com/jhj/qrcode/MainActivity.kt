0d599ff4ab340d60439e86913b4738983954d9ce
jhj24 <1406929846@qq.com>
2018-12-15 18:02:03 +0800

    commit

--- app/src/main/java/com/jhj/qrcode/MainActivity.kt
+++ app/src/main/java/com/jhj/qrcode/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.jhj.qrcode
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.jhj.zxing.QRCodeScanActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_camera.setOnClickListener {
+            startActivity<QRCodeScanActivity>()
+        }
     }
 }
--- zxing/src/main/java/com/jhj/zxing/QRCodeScanActivity.kt
+++ zxing/src/main/java/com/jhj/zxing/QRCodeScanActivity.kt
@@ -1,4 +1,35 @@
 package com.jhj.zxing
 
-class QRCodeScanActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.support.v4.content.FileProvider
+import android.support.v7.app.AppCompatActivity
+import java.io.File
+
+class QRCodeScanActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.actiivty_qrcode_scan)
+
+        val intentToTakePhoto = Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // 指定照片存储位置为sd卡本目录下
+        // 这里设置为固定名字 这样就只会只有一张temp图 如果要所有中间图片都保存可以通过时间或者加其他东西设置图片的名称
+        // File.separator为系统自带的分隔符 是一个固定的常量
+
+        val mTempPhotoPath = "${Environment.getExternalStorageDirectory()}${File.separator} +  photo.jpeg ";
+        // 获取图片所在位置的Uri路径    *****这里为什么这么做参考问题2*****
+        /*imageUri = Uri.fromFile(new File(mTempPhotoPath));*/
+        val imageUri = FileProvider.getUriForFile(
+            this,
+            applicationContext.getPackageName() + "my.provider",
+            File(mTempPhotoPath)
+        );
+        //下面这句指定调用相机拍照后的照片存储的路径
+        intentToTakePhoto.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intentToTakePhoto, 1001);
+
+    }
 }
